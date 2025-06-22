fa27343de7a8462a5f0f60674b01404a9a9d08e3
Shivam Gupta <sg4781778@gmail.com>
2025-06-22 16:36:39 +0530

    data-ingestion completed

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -11,6 +11,7 @@ class CustomException(Exception):
         _, _, exc_tb = error_detail.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
+        logger.info(f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{error_message}]")
         return f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{error_message}]"
     
     def __str__(self):
